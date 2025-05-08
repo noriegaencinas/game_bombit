ef10eb30142b9a90187ae5814ec8750377416889
Luis Noriega <91396191+noriegaencinas@users.noreply.github.com>
2025-05-07 23:02:17 -0700

    Add files via upload

--- Scripts/camera_follow.gd
+++ Scripts/camera_follow.gd
@@ -1,15 +1,1 @@
 extends Camera2D
-
-@export var node_to_follow: Node2D
-
-var offset_camera_x = 32
-
-var initial_x: float
-
-func _ready() -> void:
-	initial_x = (get_viewport_rect().size.x / 2) / zoom.x - offset_camera_x
-	if node_to_follow != null:
-		node_to_follow.tree_exiting.connect(func(): set_process(false))
-
-func _process(delta: float) -> void:
-	position.x = node_to_follow.position.x + initial_x
